7d271b88ca2b824f758dcf3fed5a5c971efbe226
Administrator <406798106@qq.com>
2016-07-11 01:19:44 +0800

    增加功能优化UI 增加用户体验

--- www/js/app.js
+++ www/js/app.js
@@ -5,7 +5,7 @@
 // the 2nd parameter is an array of 'requires'
 // 'starter.services' is found in services.js
 // 'starter.controllers' is found in controllers.js
-angular.module('starter', ['ionic', 'starter.controllers', 'starter.services', 'starter.config', 'ngCordova'])
+angular.module('starter', ['ionic', 'starter.controllers', 'starter.services', 'starter.config','starter.directive', 'ngCordova'])
 
   .run(function ($ionicPlatform, $rootScope, $ionicPopup, $location, $ionicHistory) {
     $ionicPlatform.ready(function () {
--- www/js/directive.js
+++ www/js/directive.js
@@ -1,13 +1,24 @@
 angular.module('starter.directive', [])
-  .directive('hideTabs',function($rootScope){
+  .directive('hideTabs', function ($rootScope) {
     return {
-      restrict:'AE',
-      link:function($scope){
+      restrict: 'AE',
+      link: function ($scope) {
         $rootScope.hideTabs = 'tabs-item-hide';
         //监听$destory事件,这个事件会在页面发生跳转的时候触发
-        $scope.$on('$destroy',function(){
+        $scope.$on('$destroy', function () {
           $rootScope.hideTabs = ' ';
         })
+      }
+    }
+  })
+  .directive('hideShow', function () {
+    return {
+      restrict: 'A',
+      link: function (scope, element, attrs) {
+        scope.showme = true;
+        scope.toggle = function () {//每次点击调用此方法都让scope.showme值反转1次
+          scope.showme = !scope.showme;
+        }
       }
     }
   })
