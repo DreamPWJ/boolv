8fa429281a9ba0276a83604e6a3a6720d332e8fc
406798106@qq.com <406798106@qq.com>
2016-09-29 14:53:31 +0800

    增加插件

--- plugins/cordova-plugin-statusbar/src/android/StatusBar.java
+++ plugins/cordova-plugin-statusbar/src/android/StatusBar.java
@@ -59,7 +59,7 @@ public class StatusBar extends CordovaPlugin {
                 window.clearFlags(WindowManager.LayoutParams.FLAG_FORCE_NOT_FULLSCREEN);
 
                 // Read 'StatusBarBackgroundColor' from config.xml, default is #000000.
-                setStatusBarBackgroundColor(preferences.getString("StatusBarBackgroundColor", "#000000"));
+                setStatusBarBackgroundColor(preferences.getString("StatusBarBackgroundColor", "#11c1f3"));
             }
         });
     }
