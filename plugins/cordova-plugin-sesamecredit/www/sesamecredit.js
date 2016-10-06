283671fd671743fe1ca91692c904be0aaf6715e2
406798106@qq.com <406798106@qq.com>
2016-10-06 19:17:20 +0800

    增加功能优化样式 UI  增加用户体验

---
+++ plugins/cordova-plugin-sesamecredit/www/sesamecredit.js
@@ -0,0 +1,19 @@
+/**
+ * 芝麻信用插件
+ *  潘维吉
+ */
+
+var exec = require('cordova/exec');
+/**
+ * 芝麻信用
+ */
+var SesameCredit =  {
+  test: function() {
+    alert("测试cordova插件");
+  }
+};
+
+module.exports = SesameCredit;
+
+
+
