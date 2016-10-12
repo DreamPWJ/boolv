4bfbf7acdbaed4a16a23cc2099f4924dc2bd5e84
406798106@qq.com <406798106@qq.com>
2016-10-12 11:19:40 +0800

    增加功能优化样式 UI  增加用户体验

--- www/js/config.js
+++ www/js/config.js
@@ -1,15 +1,16 @@
 /**
  * Created by pwj on 2016/5/23.
+ * 系统接口常量配置
  */
 var configMod = angular.module("starter.config", []);
 
 configMod.constant("BooLv", {
-  "name": "BooLv",
-  "debug": true,
-  "api": "http://a.boolv.com",
-  'siteUrl': "http://a.boolv.com",
-  'imgUrl': "http://f.boolv.com",
-  'moblileApi':"http://m.boolv.com",
-  'version': '1.0.9'
+  "name": "BooLv", //项目名称
+  "debug": true, //调试标示 暂无使用
+  "api": "http://a.boolv.com",//接口服务地址  使用
+  'siteUrl': "http://a.boolv.com",//仓库地址 暂无使用
+  'imgUrl': "http://f.boolv.com",//图片地址 暂无使用
+  'moblileApi':"http://m.boolv.com",//手机端服务  使用（分享链接展示调用）
+  'version': '1.0.9' //版本号
 });
 
