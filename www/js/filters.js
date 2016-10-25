e33960b670e76ec61646c73294de04af93942864
406798106@qq.com <406798106@qq.com>
2016-10-25 21:00:34 +0800

    增加功能优化样式 UI  增加用户体验

--- www/js/filters.js
+++ www/js/filters.js
@@ -10,6 +10,9 @@ angular.module('starter.filter', [])
   })
   .filter('hidepartinfo', function () { //隐藏部分信息 如手机  188****2302  潘**
     return function (str, type) {
+      if(!str){
+        return;
+      }
       if (type == 'name') { //姓名信息
         return str.replace(str.length>=3?str.substr(1,2):str.substr(1,1), str.length>=3?'**':'*');
       }
