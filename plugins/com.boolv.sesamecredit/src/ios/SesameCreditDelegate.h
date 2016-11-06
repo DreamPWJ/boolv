684242ab10ee1506468ed3a9aff2d351124ba8c3
406798106@qq.com <406798106@qq.com>
2016-11-06 15:07:43 +0800

    增加插件

---
+++ plugins/com.boolv.sesamecredit/src/ios/SesameCreditDelegate.h
@@ -0,0 +1,17 @@
+//
+//  SesameCreditDelegate.h
+//  CreditSDKMerchantDemo
+//
+//  Created by leodi on 15/11/4.
+//  Copyright © 2015年 leodi. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface SesameCreditDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
