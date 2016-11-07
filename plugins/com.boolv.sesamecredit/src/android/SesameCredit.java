2cf61650b5f68d3a55715ef450a8885bc12d8c26
406798106@qq.com <406798106@qq.com>
2016-11-07 17:27:52 +0800

    增加插件

--- plugins/com.boolv.sesamecredit/src/android/SesameCredit.java
+++ plugins/com.boolv.sesamecredit/src/android/SesameCredit.java
@@ -15,7 +15,9 @@ import org.apache.cordova.CordovaInterface;
 import org.apache.cordova.CordovaPlugin;
 import org.apache.cordova.CordovaWebView;
 import org.apache.cordova.CordovaArgs;
+import org.json.JSONArray;
 import org.json.JSONException;
+import org.json.JSONObject;
 import android.content.Intent;
 import android.app.Activity;
 import android.os.Bundle;
@@ -67,7 +69,7 @@ public class SesameCredit extends CordovaPlugin {
    * @return True if the action was valid, false otherwise.
    */
   @Override
-  public boolean execute(String action, CordovaArgs args,final CallbackContext callbackContext) throws JSONException {
+  public boolean execute(String action, CordovaArgs args, final CallbackContext callbackContext) throws JSONException {
     Log.i(TAG, "执行的方法是: " + action);
 /*    Activity activity = this.cordova.getActivity();
     Window window = activity.getWindow();*/
@@ -91,27 +93,34 @@ public class SesameCredit extends CordovaPlugin {
   }
 
   //传入参数  请求芝麻信用授权
-  private void doCreditRequest(String params, String appId, String sign, Activity activity,final CallbackContext callbackContext) {
+  private void doCreditRequest(String params, String appId, String sign, Activity activity, final CallbackContext callbackContext) {
     //extParams参数可以放置一些额外的参数，例如当biz_params参数忘记组织auth_code参数时，可以通过extParams参数带入auth_code。
     //不过建议auth_code参数组织到biz_params里面进行加密加签。
     Map<String, String> extParams = new HashMap<String, String>();
     //extParams.put("auth_code", "M_FACE");
     Toast.makeText(cordova.getActivity(), "博绿网芝麻信用授权中", Toast.LENGTH_SHORT).show();
+    //返回数据json接收
+    final JSONObject json = new JSONObject();
     try {
       //请求授权
       //应用在调用 SDK 提供的接口时,将实现了对应回调接口的实例传入。当 SDK 的接口调用完成后,如授权 调用完成后,会回调传入的接口实例。
       CreditAuthHelper.creditAuth(activity, appId, params, sign, extParams, new ICreditListener() {
         @Override
         public void onComplete(Bundle result) {
+
           //从result中获取params参数,然后解析params数据,可以获取open_id。
           if (result != null) {
             Set<String> keys = result.keySet();
             for (String key : keys) {
               Log.d(TAG, key + " = " + result.getString(key));
-              if("params".equals(key)){ //params 商家服务器端解密后的值的格式是 key1=value1&key2=value2 包含27位的芝麻客户open_id
-                callbackContext.success(result.getString(key).toString());
+              //params 商家服务器端解密后的值的格式是 key1=value1&key2=value2 包含27位的芝麻客户open_id
+              try {
+                json.put(key, result.getString(key).toString());
+              } catch (JSONException e) {
+                e.printStackTrace();
               }
             }
+            callbackContext.success(json.toString());
           }
         }
 
