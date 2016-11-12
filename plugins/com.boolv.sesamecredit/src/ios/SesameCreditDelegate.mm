f34a69b53876923069a30ba94ccfb379f6edc12f
406798016@qq.com <a18863302302>
2016-11-12 19:03:14 +0800

    IOS平台相关的特性

--- plugins/com.boolv.sesamecredit/src/ios/SesameCreditDelegate.mm
+++ plugins/com.boolv.sesamecredit/src/ios/SesameCreditDelegate.mm
@@ -7,25 +7,20 @@
 
 @implementation SesameCreditDelegate
 
+//芝麻授权初始化方法
 - (void)pluginInitialize {
   //ALCreditService是IOS SDK的功能入口，所有的接口调用都需要通过ALCreditService进行调用
   [[ALCreditService sharedService] resgisterApp];
-    self.window = [[UIWindow alloc] initWithFrame:[[UIScreen mainScreen] bounds]];
+
 }
 
-//芝麻授权回调返回结果数据
+//芝麻授权回调返回结果数据方法
 - (void)result:(NSMutableDictionary*)dic{
     NSLog(@"芝麻授权回调返回结果数据result ");
-
-    NSString* system  = [[UIDevice currentDevice] systemVersion];
-    if([system intValue]>=7){
-        self.navigationController.navigationBar.barTintColor = [UIColor whiteColor];
-    }
 
 }
  //芝麻授权方法
 - (void)sesamecredit:(CDVInvokedUrlCommand*)command{
-
 
    // 商户需要从服务端获取
     NSString* params = [command.arguments objectAtIndex:0];
@@ -35,7 +30,6 @@
     NSString* appId = @"1000697"; //博绿网 芝麻商户应用ID
 
    [[ALCreditService sharedService] queryUserAuthReq:appId sign:sign params:params extParams:nil selector:@selector(result:) target:self];
-
 
    CDVPluginResult* pluginResult = nil;
    if (appId != nil && [appId length] > 0) {
@@ -48,6 +42,7 @@
     } else {
         pluginResult = [CDVPluginResult resultWithStatus:CDVCommandStatus_ERROR];
     }
+
     //回调方法
     [self.commandDelegate sendPluginResult:pluginResult callbackId:command.callbackId];
 
